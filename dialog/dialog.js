2e80167e8501a18fb1b1cdc195ccc67b5e18b046
JQSC <1222377305@qq.com>
2016-12-22 21:12:26 +0800

    弹窗中加入拖拽功能

--- dialog/dialog.js
+++ dialog/dialog.js
@@ -45,6 +45,9 @@ Dialog.prototype.create=function(){
     this.dialog.appendChild( this.dialog_header);
     this.dialog.appendChild( this.dialog_body);
     this.dialog.appendChild( this.dialog_footer)
+
+    //加入拖拽
+    dialogDrag()
 };
 
 Dialog.prototype.close=function(){
@@ -61,8 +64,99 @@ Dialog.prototype.close=function(){
     };
 } ;
 
+function dialogDrag(){
+    var oDrag=new Drag();
+    oDrag.init({
+        id:'dialog'
+    });
+    bindEvent(oDrag,'Down',function(){
+        document.title="可拖拽"
+    });
+}
+
 function extend(obj1,obj2){
     for(var attr in obj2){
         obj1[attr]=obj2[attr]
     }
+}
+
+///拖拽
+function Drag(){
+    this.oDiv='';
+    this.disX=0;
+    this.disY=0;
+    this.settings={
+        Down:function(){},
+        Up:function(){}
+    }
+}
+
+Drag.prototype.init=function(option){
+
+    var This=this;
+    extend(this.settings,option)
+    this.oDiv=document.getElementsByClassName(option.id)[0];
+    this.oDiv.onmousedown=function(ev){
+
+        var ev=ev || window.event;
+        This.fnDown(ev);
+
+        document.onmousemove=function(ev){
+            var ev=ev||window.event;
+            This.fnMove(ev)
+            // This.settings.Down()
+            clickEvent(This,'Down')
+        };
+
+        document.onmouseup=function(){
+
+            document.onmousemove=null;
+            document.onmouseup=null;
+            This.settings.Up();
+            clickEvent(This,'Up')
+        };
+        return false
+    };
+};
+Drag.prototype.fnDown=function(ev){
+    this.disX=ev.clientX-this.oDiv.offsetLeft;
+    this.disY=ev.clientY-this.oDiv.offsetTop
+};
+Drag.prototype.fnMove=function(ev){
+    var L = ev.clientX - this.disX;
+    var T = ev.clientY - this.disY;
+    if(L<0){
+        L = 0;
+    }
+    else if(L>document.documentElement.clientWidth - this.oDiv.offsetWidth){
+        L = document.documentElement.clientWidth - this.oDiv.offsetWidth;
+    }
+    if(T<0){
+        T=0
+    }else if(T>document.documentElement.clientHeight - this.oDiv.offsetHeight){
+        T=document.documentElement.clientHeight - this.oDiv.offsetHeight
+    }
+    this.oDiv.style.left=L+'px';
+    this.oDiv.style.top=T+'px';
+};
+
+function bindEvent(obj,events,fn){
+
+    obj.listeners=obj.listeners||{};
+    obj.listeners[events]=obj.listeners[events]||[];
+    obj.listeners[events].push(fn)
+
+    if (obj.addEventListener) {                     //所有主流浏览器，除了 IE 8 及更早 IE版本
+        obj.addEventListener(events, fn,false);
+    } else if (obj.attachEvent) {                  // IE 8 及更早 IE 版本
+        obj.attachEvent('on'+events, fn);
+    }
+}
+function clickEvent(obj,events){
+    if(obj.listeners[events]){
+        for(var i=0;i<obj.listeners[events].length;i++){
+            obj.listeners[events][i]();
+        }
+    }
+
 }
